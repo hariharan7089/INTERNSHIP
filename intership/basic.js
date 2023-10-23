0d7f79d5a674d4f89c028ffd2a09989f810488aa
hariharan7089 <145661554+hariharan7089@users.noreply.github.com>
2023-10-23 02:36:04 -0700

    Add files via upload

---
+++ intership/basic.js
@@ -0,0 +1,11 @@
+function calculateSum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    }
+    return sum;
+    }
+    const numbers = [1, 2, 3, 4, 5];
+    const result = calculateSum(numbers);
+    console.log(result);
+    
